a2913a24576345bb59347638bcb0370518f24961
zhuyuhang <zyhwitness23@gmail.com>
2025-06-28 12:08:34 +0800

    feat: canCreate、createBean

--- src/main/java/com/demo/spring/ApplicationContext.java
+++ src/main/java/com/demo/spring/ApplicationContext.java
@@ -15,16 +15,30 @@ public class ApplicationContext {
     }
 
     public void initContext(String packageName) {
-        // ApplicationContext.class.getClassLoader().getResource("");
-        List<Class<?>> componentClassList = scanPackage(packageName).
-                stream().
-                filter(aClass -> aClass.isAnnotationPresent(Component.class)).toList();
+        scanPackage(packageName).stream().filter(this::canCreate).map(this::wrapper).forEach(this::createBean);
+//        List<BeanDefinition> list =
+//                scanPackage(packageName).stream().filter(this::scanCreate).map(this::wrapper).toList();
+
+//        ApplicationContext.class.getClassLoader().getResource("");
+//        List<Class<?>> componentClassList = scanPackage(packageName).
+//                stream().
+//                filter(aClass -> aClass.isAnnotationPresent(Component.class)).toList();
     }
 
-    protected boolean scanCreate(Class<?> type){
+    // 判断什么样的类可以创建bean
+    protected boolean canCreate(Class<?> type) {
         return type.isAnnotationPresent(Component.class);
     }
 
+    // 转换成BeanDefinition
+    protected BeanDefinition wrapper(Class<?> type) {
+        return new BeanDefinition(type);
+    }
+
+    // 通过BeanDefinition创建bean
+    protected void createBean(BeanDefinition beanDefinition) {
+
+    }
 
     // 扫描指定包下的所有类，不一定是一种类型的类
     private List<Class<?>> scanPackage(String packageName) {
